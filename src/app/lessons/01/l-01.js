f6b0ea2e7d18a262a76d1eb753cc800e727ea293
Stepan Protsiv <stepan.protsiv@synapse.com>
2018-05-24 16:49:44 +0300

    l-01 result

---
+++ src/app/lessons/01/l-01.js
@@ -0,0 +1,31 @@
+
+//console.log('\nLesson 01 - Homework:\n\n!!! Please answer the questions in this file !!!\n');
+
+// Lesson 01 Homework - Start
+
+// Task 01.01 - 1 point
+// Question: Can JavaScript file contain non-executable code?
+// Answer: Yes - it is comments
+
+// Task 01.02 - 2 points
+// Question: How we can tell the JS interpreter to use strict mode?
+// Answer: 'use strict';
+
+// Task 01.03 - 2 points
+// Q: What simple data types are used in JS?
+// A: string, bolean, number, null, undefined
+
+// Task 01.04 - 2 points
+// Q: What complex data types are used in JS?
+// A: functions, objects
+
+// Task 01.05 - 3 points
+// Q: Please write a function which calculates the difference between two numbers
+// A: 
+function difference (a, b){
+    return (a - b);
+}
+console.log(difference (5, 20));
+
+
+// Lesson 01 Homework - End
