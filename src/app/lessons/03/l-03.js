02ede7c3703779c82f0ef6fe13c6a8d2e27d743f
Stepan Protsiv <stepan.protsiv@synapse.com>
2018-06-08 13:49:03 +0300

    homework lesson 3

--- src/app/lessons/03/l-03.js
+++ src/app/lessons/03/l-03.js
@@ -210,25 +210,31 @@ console.log('\nTask 03.01:');
 // Task 03.01
 // Points: 2
 // In code above, we created 'lesson' object. Add 'date' property to it, having null value.
-// TODO: write code here
+lesson.date = null;
+console.log(lesson.date);
 
 console.log('\nTask 03.02');
 // Task 03.02
 // Points: 3
 // Add getInfo method to the lesson object, it should log lesson info to the console: lesson name, id and date.
-// TODO: write code here
+student.getInfo = function(){
+    console.log("lesson name: " + lesson.name + "\n id: " + lesson.id + "\n date: " + lesson.date);
+}
+student.getInfo();
 
 console.log('\nTask 03.03');
 // Task 03.03
 // Points: 2
 // In code above, we defined the 'student' object. 
 // Add 'birthday' property to it, set it's value to new Date(1991,8,24);
-// TODO: write code here
+student.birthday = new Date(1991,8,24);
+console.log(student.birthday);
+
 
 console.log('\nTask 03.04: TODO: Review this file and make sure you understand it.');
 // Task 03.04
 // Points: 1
 // TODO: if you performed this task, please uncomment next line:
-// console.log('I\'ve reviewed the l-03.js file and I understand it');
+console.log('I\'ve reviewed the l-03.js file and I understand it');
 
 console.log('\nLesson 03 - Homework End.');
